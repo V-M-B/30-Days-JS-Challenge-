8f3ce6183fc0592fb1b49d12c4ad3b566a77b996
V-M-B <vmbvarun@gmail.com>
2024-07-17 00:04:24 +0530

    Day-4 JS Challenge Completed.

---
+++ Day-4/4th.js
@@ -0,0 +1,89 @@
+//q1 1-10 using for
+// for(let i=1;i<=10;i++)
+// {
+//     console.log(i)
+// }
+
+//q2 5 table using for
+// for(var i=1;i<=10;i++)
+//     {
+//         //console.log("5*",i,"=",5*i)
+//         console.log(`5 * ${i} =${i*5}`)
+//     }
+
+// sum of 1 -10 using while
+// var i=1;
+// var sum=0
+// while(i<=10)
+// {   sum=sum+i;//sum+=i;
+//     i++; 
+// }
+// console.log("Sum is ",sum)
+
+
+//q3 10 - 1 using while loop
+// var i=10;
+// var sum=0;
+// while(i>0)
+// {
+ 
+//     console.log(i)
+//     i--;
+   
+// }
+
+//q4 do-while 1-5
+// let number=1;
+// do{
+//     console.log(number)
+//     number++;
+// }while(number<=5)
+
+
+//factorial using do while
+// function factorial(){
+// let n=5;
+// let i=1;
+// let result=1;
+// do{
+// result*=i//r=r*i
+// i++
+// }while(n>=i)
+//     return result;
+
+// }
+// console.log(factorial())
+
+
+//star 
+// let str ="";
+// for (let i = 1; i <=5; i++) {
+//    for (let index = 1; index <= i; index++) {
+//     str+="*";
+    
+//    }
+//     str+="\n";
+// }
+// console.log(str)
+
+//break and continue
+// for(let i=1;i<=10;i++)
+// {   if(i==5){
+//     continue;
+// }
+    
+//     console.log(i)
+
+// }
+
+
+//break 7
+// for(let i=1;i<=10;i++)
+// {   
+//     //console.log(i)
+//     if(i==7){
+//         console.log(i)
+//     break;
+// }   
+// console.log(i)
+// }
