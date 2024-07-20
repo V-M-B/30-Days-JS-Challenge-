87dba85efcc4c2202cd31e82ae26bd14366e6e0f
V-M-B <vmbvarun@gmail.com>
2024-07-20 21:17:25 +0530

    Day-8 JS Challenge completed.

---
+++ Day-8/8th.js
@@ -0,0 +1,15 @@
+//Q1 Use template literals to create a string that includes variables for a person’s 
+//name and age, and log the string to the console.
+let name = "Alice";
+let age = 30;
+let introduction = `My name is ${name} and I am ${age} years old.`;
+console.log(introduction);
+
+
+//Q2 Create a multi-line string using 
+//template literals and log it to the console.
+
+let multiLineString = `This is a multi-line string.
+It spans multiple lines.
+Isn't it cool?`;
+console.log(multiLineString);
